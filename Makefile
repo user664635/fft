86bd9cc6a4b10947bc61d194ec8d99ba59b60c50
user664635 <myy12358@gmail.com>
2025-07-23 16:44:12 +0800

    sin

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CC = clang
-CFLAGS = -std=gnu2y -lSDL3
+CFLAGS = -std=gnu2y -O3 -march=native -fuse-ld=lld -lm -lSDL3
 all: main
 	./main
--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <math.h>
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
+constexpr int weight = 1920, height = 1080;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   if (!SDL_Init(SDL_INIT_VIDEO))
     return SDL_APP_FAILURE;
-  if (!SDL_CreateWindowAndRenderer("fft", 640, 480, 0, &window, &renderer))
+  if (!SDL_CreateWindowAndRenderer("fft", weight, height, 0, &window,
+                                   &renderer))
     return SDL_APP_FAILURE;
   return SDL_APP_CONTINUE;
 }
@@ -19,9 +22,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   return SDL_APP_CONTINUE;
 }
 
+float f(float x) {
+  float f0 = 5e3, p0 = 0;
+  return sinpi(f0 * x / 2 + p0);
+}
 SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 0);
   SDL_RenderClear(renderer);
+
+  constexpr int n = 1024;
+  float data[n];
+  for (int i = 0; i < n; ++i)
+    data[i] = f(i / 1e6);
+
+  float h = height * .25;
+  SDL_FPoint arr[n];
+  for (int i = 0; i < n; ++i)
+    arr[i].y = data[i] * -h + h, arr[i].x = i;
+
+  SDL_SetRenderDrawColorFloat(renderer, 1, 1, 1, 1);
+  SDL_RenderLines(renderer, arr, n);
   SDL_RenderPresent(renderer);
   return SDL_APP_CONTINUE;
 }
