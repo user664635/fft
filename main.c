c9d588fe039e497729d1ca1dd1078493657c6b85
user664635 <myy12358@gmail.com>
2025-07-25 02:45:47 +0800

    fft

--- main.c
+++ main.c
@@ -8,12 +8,37 @@
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
-constexpr int w = 2048, h = 1000, n = 64;
+constexpr int w = 2048, h = 1000, n = 64, logn = __builtin_ctz(n);
 
-static float *in;
-static float complex *out;
 static fftwf_plan p;
 static SDL_FPoint arr[w];
+
+typedef float f32;
+typedef complex float c32;
+static int rev[n];
+static f32 *in;
+static c32 *out, rot[n];
+static c32 out1[n];
+void fft_init() {
+  for (int i = 0; i < n; ++i)
+    rev[i] = __builtin_bitreverse16(i) >> (16 - logn),
+    rot[i] = cexp(-2i * M_PI * i / n);
+}
+void fft(f32 *in, c32 *out) {
+  for (int i = 0; i < n; ++i)
+    out[rev[i]] = in[i];
+  for (int i = 0; i < logn; ++i) {
+    int m = 1 << i;
+    for (int j = 0; j < n; j += m << 1)
+      for (int k = 0; k < m; ++k) {
+        c32 t = rot[k << (logn - i - 1)] * out[j + k + m];
+        c32 u = out[j + k];
+        out[j + k] = u + t;
+        out[j + k + m] = u - t;
+      }
+  }
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   if (!SDL_Init(SDL_INIT_VIDEO))
     return SDL_APP_FAILURE;
@@ -22,17 +47,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   in = fftwf_alloc_real(n);
   out = fftwf_alloc_complex(n);
   p = fftwf_plan_dft_r2c_1d(n, in, out, FFTW_MEASURE);
+  fft_init();
   return SDL_APP_CONTINUE;
 }
 
-static float f0 = 50e3, p0, f1 = 250e3, p1;
+static f32 f0 = 50e3, p0, f1 = 250e3, p1;
+f32 f(f32 x) { return sinpi(f0 * 2 * x + p0) + sinpi(f1 * 2 * x + p1); }
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   switch (event->type) {
   case SDL_EVENT_QUIT:
     return SDL_APP_SUCCESS;
   case SDL_EVENT_MOUSE_MOTION: {
     auto tmp = event->motion;
-    float x = tmp.x / w, y = 1 - tmp.y / h;
+    f32 x = tmp.x / w, y = 1 - tmp.y / h;
     if (tmp.state != 1)
       goto end;
     if (x < .5) {
@@ -53,16 +80,6 @@ end:
 }
 
 #define clk __builtin_readcyclecounter()
-static float complex out1[n];
-float f(float x) { return sinpi(f0 * 2 * x + p0) + sinpi(f1 * 2 * x + p1); }
-void fft(float *in, float complex *out) {
-  for (int i = 0; i < n / 2; ++i) {
-    float complex s = 0;
-    for (int j = 0; j < n / 2; ++j)
-      s += in[j] * cexpf(M_PI * -2i * i * j / n);
-    out[i] = s;
-  }
-}
 static long st0, st1;
 static double cnt;
 SDL_AppResult SDL_AppIterate(void *appstate) {
@@ -70,7 +87,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_RenderClear(renderer);
 
   for (int i = 0; i < n; ++i)
-    out1[i] = in[i] = f(i / 1e6);
+    in[i] = f(i / 1e6);
   ++cnt;
   long t0 = clk;
   fftwf_execute(p);
@@ -81,7 +98,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   st0 += dt0, st1 += dt1;
   printf("%f,%f\n", st0 / cnt, st1 / cnt);
 
-  float y = h * .25;
+  f32 y = h * .25;
   for (int i = 0; i < w; ++i)
     arr[i].x = i;
   for (int i = 0; i < w; ++i)
@@ -90,7 +107,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_RenderLines(renderer, arr, w);
 
   for (int i = 0; i < n; ++i)
-    arr[i].x = (float)i * w / n;
+    arr[i].x = (f32)i * w / n;
   for (int i = 0; i < n; ++i)
     arr[i].y = in[i] * -y / 2 + y;
   SDL_SetRenderDrawColorFloat(renderer, 1, 1, 1, .5);
